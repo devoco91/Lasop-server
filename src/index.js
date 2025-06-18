3e0d94910c980fb1679ada71b11f622e14f34a62
David <ashuaib1991@gmail.com>
2025-06-18 15:30:51 +0100

    port added

--- src/index.js
+++ src/index.js
@@ -251,6 +251,7 @@ app.get('/', (req, res) => {
 connection();
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server running on port ${PORT}`));
+
 
 module.exports = app;
