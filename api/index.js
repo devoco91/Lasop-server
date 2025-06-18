dab35ed7c8615d6ae84247aa800b08c090fad49c
David <ashuaib1991@gmail.com>
2025-06-18 15:20:56 +0100

    ready for fly

--- api/index.js
+++
@@ -1,5 +0,0 @@
-// api/index.js
-const app = require('../src/index');
-const serverless = require('serverless-http');
-
-module.exports = serverless(app);
--- src/index.js
+++ src/index.js
@@ -102,10 +102,10 @@ const updateStudentWithoutOtherName = require("./routes/student/updateStudentOth
 
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
 
 // Admin / Accountant / Super admin
 app.post('/user', signUser);
@@ -248,6 +248,9 @@ app.get('/', (req, res) => {
   res.status(200).send('API is working ✅');
 });
 
-connection(); // ✅ this initializes MongoDB
+connection();
 
-module.exports = app; // 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+module.exports = app;
